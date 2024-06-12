c7f950c58be6c8848be06bd3c792422bf2190922
Gabriel-Pereira1788 <gabriel.pereira1788@gmail.com>
2024-06-12 00:25:02 -0300

    fix: default api host

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,6 +1,8 @@
 import {API_HOST} from '@env';
 import axios from 'axios';
-export const BASE_URL = API_HOST;
+
+export const BASE_URL = API_HOST ? API_HOST : 'http://localhost:3000/';
+
 export const api = axios.create({
   baseURL: BASE_URL,
 });
